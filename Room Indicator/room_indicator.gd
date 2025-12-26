503c038c2c1b1c4a78167c3e9099a0ed88d71595
NIK-Pro-coder <nikolai.carnaghi@gmail.com>
2025-12-26 11:33:23 +0100

    Tweaked room inidcator

--- Room Indicator/room_indicator.gd
+++ Room Indicator/room_indicator.gd
@@ -41,6 +41,44 @@ func _on_visible_screen_entered() -> void:
 func _on_visible_screen_exited() -> void:
   on_screen = false
 
+## Taken from https://www.jeffreythompson.org/collision-detection/line-rect.php 
+func lineRect(x1: float, y1: float, x2: float, y2: float, rx: float, ry: float, rw: float, rh: float) -> Vector2 :
+  # check if the line has hit any of the rectangle's sides
+  # uses the Line/Line function below
+  var left: Vector2 =   lineLine(x1,y1,x2,y2, rx,ry,rx, ry+rh)
+  var right: Vector2 =  lineLine(x1,y1,x2,y2, rx+rw,ry, rx+rw,ry+rh)
+  var top: Vector2 =    lineLine(x1,y1,x2,y2, rx,ry, rx+rw,ry)
+  var bottom: Vector2 = lineLine(x1,y1,x2,y2, rx,ry+rh, rx+rw,ry+rh)
+
+  # if ANY of the above are true, the line
+  # has hit the rectangle
+  if left != Vector2(PI, PI) :
+    return left
+  if right != Vector2(PI, PI) :
+    return right
+  if top != Vector2(PI, PI) :
+    return top
+  if bottom != Vector2(PI, PI) :
+    return bottom
+    
+  return Vector2(PI, PI)
+
+# LINE/LINE
+func lineLine(x1: float, y1: float, x2: float, y2: float, x3: float, y3: float, x4: float, y4: float) -> Vector2 :
+  # calculate the direction of the lines
+  var uA: float = ((x4-x3)*(y1-y3) - (y4-y3)*(x1-x3)) / ((y4-y3)*(x2-x1) - (x4-x3)*(y2-y1));
+  var uB: float = ((x2-x1)*(y1-y3) - (y2-y1)*(x1-x3)) / ((y4-y3)*(x2-x1) - (x4-x3)*(y2-y1));
+
+  # if uA and uB are between 0-1, lines are colliding
+  if (uA >= 0 && uA <= 1 && uB >= 0 && uB <= 1) :
+    # optionally, draw a circle where the lines meet
+    var intersectionX: float = x1 + (uA * (x2-x1))
+    var intersectionY: float = y1 + (uA * (y2-y1))
+    
+    return Vector2(intersectionX, intersectionY)
+  
+  return Vector2(PI, PI)
+
 func _process(_delta: float) -> void:
   visible = len(get_tree().get_nodes_in_group("enemy")) == 0
   l.visible = !on_screen and visible
@@ -65,9 +103,19 @@ func _process(_delta: float) -> void:
   if not p :
     return
   
-  global_position = p.global_position
-  global_position = global_position + (DOOR_POS - global_position).normalized() * 250
+  global_position = DOOR_POS
 
+  var bounds: Vector2 = get_viewport().get_visible_rect().size
+  var cam_pos: Vector2 = get_viewport().get_camera_2d().get_parent().global_position
+  
+  global_position = lineRect(
+    global_position.x, global_position.y, 
+    cam_pos.x, cam_pos.y, 
+    
+    cam_pos.x - bounds.x + 100, cam_pos.y - bounds.y + 100, 
+    bounds.x * 2 - 200, bounds.y * 2 - 200
+  )
+  
   var delta = (DOOR_POS - global_position).normalized() * 50
 
   l.points = [global_position - delta, global_position - delta * 2.5]
